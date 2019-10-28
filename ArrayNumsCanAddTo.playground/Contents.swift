c217c2a459f6c740a5b82db1449de2ded733617a
Viranchee <viranchee@outlook.com>
2019-10-28 18:04:57 +0530

    Updated Method naming convention

--- ArrayNumsCanAddTo.playground/Contents.swift
+++ ArrayNumsCanAddTo.playground/Contents.swift
@@ -11,19 +11,18 @@
 
 
 /// This function outputs if the number can be made up of 2 digits in the array
-/// - Parameter array: The Array
+/// - Parameter list: The Array
 /// - Parameter number: The Number to be checked
 /// - Warning: The solution can fail when an element repeats in the array
 /// - Discussion: To have a single pass through the array, we convert the array to Set as operations on Set such as `Set.contains(element)` take O(1) while `Array.contains(element)` take O(n) time. Once an element is extracted from the Set, I remove it from the set, just so as not to have `contains` return true on it. Once a match is found, the flag turns true and program breaks off the  loop. Else it continues and exhausts the Set. The flag is returned
 /// - Complexity: O(n). The Array is converted to set in O(n) time, while the set's value is accessed in O(1) time
-/// -
-func array(_ array: [Int], contains number: Int) -> Bool {
-  if array.isEmpty {
+func array(_ list: [Int], canSumUpTo number: Int) -> Bool {
+  if list.isEmpty {
     return false
   }
   
   // Convert to set and brute force through!
-  var setOfNumbers = Set(array)
+  var setOfNumbers = Set(list)
   var containsNumberFlag = false
   
   for currentNum in setOfNumbers {
@@ -42,4 +41,4 @@ let array1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
 let val = 32
 
 
-dump(array(array1, contains: val))
+dump(array(array1, canSumUpTo: val))
