75533a86abf7a058efba2484fdd60a0c5c6224a1
Viranchee <viranchee@outlook.com>
2019-11-20 20:41:55 +0530

    Solved 1/3 Ad-Hoc polymorphism

--- Solution2.playground/Contents.swift
+++ Solution2.playground/Contents.swift
@@ -8,4 +8,38 @@ Describe and give an example of each of the following types of polymorphism:
     Subtype polymorphism
 */
 
+// AD HOC Polymorphism
 
+/*
+From What I've read, Adhoc polymorphism refers to function overloading, or operator overloading
+ 
+*/
+
+precedencegroup DoubleAdd {
+    higherThan: ComparisonPrecedence
+    associativity: left
+}
+
+infix operator ++: DoubleAdd
+
+func ++<A: Numeric> (_ lhs: A, _ rhs: A) -> A {
+    return lhs + rhs + rhs
+}
+
+func addTwice<A: Numeric>(_ lhs: A, _ rhs: A) -> A {
+    return lhs ++ rhs
+}
+
+func testAdHoc() {
+    assert(5 ++ 1 == 7, "Works with Ints")
+    assert(5.1 ++ 1 == 7.1, "Works with Floats")
+    print("Tests Passed")
+}
+
+testAdHoc()
+
+// Parametric polymorphism
+
+/*
+ Parametric p
+ */
