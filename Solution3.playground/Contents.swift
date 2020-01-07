d5a76b988bd5bacc9b5cf50d07af35c6eed8501e
Viranchee <viranchee@outlook.com>
2020-01-07 13:21:20 +0530

    Some function

---
+++ Solution3.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+var str = "Hello, playground"
+/*
+ #3 This problem was asked by Amazon.
+ 
+ Given n numbers, find the greatest common denominator between them.
+ 
+ For example, given the numbers [42, 56, 14], return 14.
+ */
+
+func getGCD(numbers: [Int]) -> Int {
+     
+    return 5
+}
